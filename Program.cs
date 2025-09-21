1ae7df791b5f984be979eb1a6ba750178027614b
Ca-Ma-Bo <cmb46699@hawkmail.hacc.edu>
2025-09-20 21:35:26 -0400

    main code for the project

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentCode
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public List<int> Grades { get; set; } = new List<int>();
+
+        public Student(string name) => Name = name;
+
+        public void AddGrade(int grade) => Grades.Add(grade);
+
+        public double GetAverageGrade() => Grades.Count > 0 ? Grades.Average() : 0.0;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new List<Student>();
+
+            while (true)
+            {
+                Console.Write("Enter student name (or done to stop): ");
+                string name = Console.ReadLine();
+
+                if (name.ToLower() == "done")
+                    break;
+
+                var student = new Student(name);
+
+                while (true)
+                {
+                    Console.Write($"Enter grade for {name} (or -1 to stop): ");
+                    int grade = int.Parse(Console.ReadLine());
+
+                    if (grade == -1)
+                        break;
+
+                    student.AddGrade(grade);
+                }
+
+                students.Add(student);
+            }
+
+            Console.WriteLine("\nClassroom Summary:");
+            foreach (var s in students)
+            {
+                Console.WriteLine($"{s.Name} - Average Grade: {s.GetAverageGrade():F1}");
+            }
+        }
+    }
+}
